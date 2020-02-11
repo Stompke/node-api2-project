c123aa89a61b9bbbfef0faa8e149a43ba6534796
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-11 13:30:35 -0800

    request to  is working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -5,7 +5,15 @@ const Posts = require("../data/db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send('good job!');
+    // res.send('good job!');
+    Posts.find()
+    .then(posts => {
+        res.status(200).json({posts})
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The posts information could not be retrieved." })
+    })
 })
 
 router.post("/", (req, res) => {
@@ -27,8 +35,29 @@ router.post("/", (req, res) => {
 })
 
 router.post("/:id/comments", (req, res) => {
-    Posts.insertComment()
+    const id = req.params.id;
+    const commentBody = req.body;
+    if(!body.text) {
+        console.log(commentBody)
+        res.res(400).json({ errorMessage: "Please provide text for the comment."})
+    } else {
+       Posts.insertComment()
+    }
 })
+
+
+router.get("/:id"), (req, res) => {
+    console.log('works????')
+    Posts.findById(req.params.id)
+    .then(post => {
+        console.log(post)
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The post information could not be retrieved."})
+    })
+}
 
 
 module.exports = router;
