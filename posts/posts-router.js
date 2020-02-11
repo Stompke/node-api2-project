e30c411e7708aab7eec85f4c58e56fcf5df4bfc2
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-11 12:34:47 -0800

    POST request to /api/posts is working

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -8,4 +8,22 @@ router.get("/", (req, res) => {
     res.send('good job!');
 })
 
+router.post("/", (req, res) => {
+    
+
+    Posts.insert(req.body)
+    .then(newPost => {
+        console.log(req.body)
+        res.status(210).json(newPost)
+    })
+    .catch(err => {
+        console.log(req.body)
+        if(!req.body.title || !req.body.contents) {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+        } else {
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        }
+    })
+})
+
 module.exports = router;
