e74ff08cb65f5e0dbb443f29636ca994b23631d7
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-11 13:09:51 -0800

    fixed if statement on Post to api/posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -10,20 +10,25 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
     
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts.insert(req.body)
+        .then(newPost => {
+            console.log(req.body)
+            res.status(210).json(newPost)
+        })
+        .catch(err => {
+            console.log(req.body)
+                res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+    }
 
-    Posts.insert(req.body)
-    .then(newPost => {
-        console.log(req.body)
-        res.status(210).json(newPost)
-    })
-    .catch(err => {
-        console.log(req.body)
-        if(!req.body.title || !req.body.contents) {
-            res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-        } else {
-            res.status(500).json({ error: "There was an error while saving the post to the database" })
-        }
-    })
 })
+
+router.post("/:id/comments", (req, res) => {
+    Posts.insertComment()
+})
+
 
 module.exports = router;
